e1dd12a9c2018479c1a0fbdaed5aaacafd6f1867
dex-studios-ug <bbshriccy@gmail.com>
2026-04-06 12:00:53 -0700

    xsx

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import {xs} from 'path';
+import {resolve} from 'path';
 
 export default defineConfig({
   plugins: [react()],
