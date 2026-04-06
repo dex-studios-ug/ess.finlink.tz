4d46409c9a4015babcaf07a3bb8551507551bb60
dex-studios-ug <bbshriccy@gmail.com>
2026-04-06 11:13:54 -0700

    multiple files

--- vite.config.js
+++ vite.config.js
@@ -17,4 +17,13 @@ export default defineConfig({
       },
     },
   },
+   build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        crdb: resolve(__dirname, 'crdb.html'),
+       
+      },
+    },
+  },
 });
